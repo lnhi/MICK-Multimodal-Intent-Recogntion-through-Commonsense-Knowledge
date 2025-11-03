eafa149bcd394ce76ed549d71f7e30f29fa84d2e
lnhi <nhiinguyen2406@gmail.com>
2025-11-03 15:24:25 +0700

    update mick

--- mick/backbones/FusionNets/MICK.py
+++ mick/backbones/FusionNets/MICK.py
@@ -86,94 +86,79 @@ class VisualCommonsenseEnrichment(nn.Module):
 class MFA(nn.Module):
     
     def __init__(self, args):
-        super().__init__()
 
+        super(MFA, self).__init__()
         self.args = args
 
-        self.visual_embedding_size = args.video_feat_dim
-        self.acoustic_embedding_size = args.audio_feat_dim
-        self.textembedding_size = args.text_feat_dim
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.destination_embedding_size = args.dst_feature_dims
+        self.visual_size = args.video_feat_dim
+        self.acoustic_size = args.audio_feat_dim
+        self.text_size = args.text_feat_dim
+        self.device = args.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.dst_feature_dims = args.dst_feature_dims
 
-        self.num_self_attention_layers = args.n_levels_self
-        self.num_cross_attention_layers = args.n_levels_cross
+        self.layers_cross = args.n_levels_cross 
+        self.layers_self = args.n_levels_self 
 
         self.dropout_rate = args.dropout_rate
-        self.cross_attention_dropout_rate = args.cross_dp_rate
-        self.num_cross_attention_heads = args.cross_num_heads
-        self.num_self_attention_heads = args.self_num_heads
+        self.cross_dp_rate = args.cross_dp_rate
+        self.cross_num_heads = args.cross_num_heads
+        self.self_num_heads = args.self_num_heads
 
-        self.self_attention = nn.TransformerEncoderLayer(
-            d_model=self.destination_embedding_size,
-            nhead=self.num_self_attention_heads
-        )
-        self.self_attention_module = nn.TransformerEncoder(
-            self.self_attention, num_layers=self.num_self_attention_layers
-        )
+        encoder_layer = nn.TransformerEncoderLayer(d_model=self.dst_feature_dims, nhead=self.self_num_heads)
+        self.self_att = nn.TransformerEncoder(encoder_layer, num_layers=self.layers_self)
 
-        self.video_to_text_cross_attention = BertCrossEncoder(
-            num_heads=self.num_cross_attention_heads,
-            hidden_size=self.destination_embedding_size,
-            dropout=self.cross_attention_dropout_rate,
-            num_layers=self.num_cross_attention_layers
-        )
-        self.audio_to_text_cross_attention = BertCrossEncoder(
-            num_heads=self.num_cross_attention_heads,
-            hidden_size=self.destination_embedding_size,
-            dropout=self.cross_attention_dropout_rate,
-            num_layers=self.num_cross_attention_layers
-        )
+        self.video2text_cross = BertCrossEncoder(self.cross_num_heads, self.dst_feature_dims, self.cross_dp_rate, n_layers=self.layers_cross)
+        self.audio2text_cross = BertCrossEncoder(self.cross_num_heads, self.dst_feature_dims, self.cross_dp_rate, n_layers=self.layers_cross)
+        
+        self.v2t_project = nn.Linear(self.visual_size, self.text_size)
 
-        self.visual_to_text_projection = nn.Linear(
-            self.visualembedding_size, self.textembedding_size
-        )
-
-        self.multimodal_fusion = nn.Sequential(
-            nn.Linear(self.destinationembedding_size*3, self.destinationembedding_size),
-            nn.Dropout(self.dropout_rate),
-            nn.GELU()
-        )
+        self.mlp_project =  nn.Sequential(
+                nn.Linear(self.dst_feature_dims*3, self.dst_feature_dims),
+                nn.Dropout(args.dropout_rate),
+                nn.GELU()
+            )
 
     def forward(self, text_feats, video_feats, audio_feats, text_mask):
-        """
-        Forward pass of the MICK model
-        """
+        # first layer : T,V,A
         bert_sent_mask = text_mask
-        text_seq = text_feats
+        text_seq = text_feats  
 
         video_seq = self.v2t_project(video_feats)
         audio_seq = audio_feats
 
-        video_mask = torch.sum(video_feats.ne(torch.zeros(video_feats[0].shape[-1])).to(self.device)).int(), dim=-1)/video_feats[0].shape[-1]
-        video_mask_len = torch.sum(video_mask, dim=1, keepdim=True)
+        video_mask = torch.sum(video_feats.ne(torch.zeros(video_feats[0].shape[-1]).to(self.device)).int(), dim=-1)/video_feats[0].shape[-1]
+        video_mask_len = torch.sum(video_mask, dim=1, keepdim=True)  
 
         video_mask_len = torch.where(video_mask_len > 0.5, video_mask_len, torch.ones([1]).to(self.device))
         video_masked_output = torch.mul(video_mask.unsqueeze(2), video_seq)
         video_rep = torch.sum(video_masked_output, dim=1, keepdim=False) / video_mask_len
-
-        audio_mask = torch.sum(audio_feats.ne(torch.zeros(audio_feats[0].shape[-1])).to(self.device)).int(), dim=-1)/audio_feats[0].shape[-1]
-        audio_mask_len = torch.sum(audio_mask, dim=1, keepdim=True)
-
-        audio_mask_len = torch.where(audio_mask_len > 0.5, audio_mask_len, torch.ones([1]).to(self.device))
+        
+        audio_mask = torch.sum(audio_feats.ne(torch.zeros(audio_feats[0].shape[-1]).to(self.device)).int(), dim=-1)/audio_feats[0].shape[-1]
+        audio_mask_len = torch.sum(audio_mask, dim=1, keepdim=True)  
+        
         audio_masked_output = torch.mul(audio_mask.unsqueeze(2), audio_seq)
         audio_rep = torch.sum(audio_masked_output, dim=1, keepdim=False) / audio_mask_len
-
-        video2text_seq = self.video2text_cross(text_seq, video_seq, video_mask)
+        
+        # second layer (V,A) --> T: V_T, A_T
         extended_video_mask = video_mask.unsqueeze(1).unsqueeze(2)
         extended_video_mask = extended_video_mask.to(dtype=next(self.parameters()).dtype)
         extended_video_mask = (1.0 - extended_video_mask) * -10000.0
+        video2text_seq = self.video2text_cross(text_seq, video_seq, extended_video_mask)
 
-        audio2text_seq = self.audio2text_cross(text_seq, audio_seq, audio_mask)
+        extended_audio_mask = audio_mask.unsqueeze(1).unsqueeze(2)
+        extended_audio_mask = extended_audio_mask.to(dtype=next(self.parameters()).dtype)
+        extended_audio_mask = (1.0 - extended_audio_mask) * -10000.0
+        audio2text_seq = self.audio2text_cross(text_seq, audio_seq, extended_audio_mask)
 
-        text_mask_len = torch.sum(bert_sent_mask, dim=1, keepdim=True)
+        text_mask_len = torch.sum(bert_sent_mask, dim=1, keepdim=True) 
 
         video2text_masked_output = torch.mul(bert_sent_mask.unsqueeze(2), video2text_seq)
         video2text_rep = torch.sum(video2text_masked_output, dim=1, keepdim=False) / text_mask_len
-
+        
         audio2text_masked_output = torch.mul(bert_sent_mask.unsqueeze(2), audio2text_seq)
         audio2text_rep = torch.sum(audio2text_masked_output, dim=1, keepdim=False) / text_mask_len
-
+        
+        # third layer: mlp->VAL
         shallow_seq = self.mlp_project(torch.cat([audio2text_seq, text_seq, video2text_seq], dim=2))
 
         return shallow_seq
@@ -181,19 +166,18 @@ class MFA(nn.Module):
 class MICK(nn.Module):
     def __init__(self, args):
         super(MICK, self).__init__()
+        self.config = BertConfig.from_pretrained(args.text_backbone)
+        self.text_encoder = CommonsenseTextEncoder(args)
+        self.commonsense_fusion = CommonsenseInferenceFusion(args)
+        self.textual_fusion = TextualCommonsenseEnrichment(args)
+        self.visual_fusion = VisualCommonsenseEnrichment(args)
+        self.mfa = MFA(args)
 
-        self._config = BertConfig.from_pretrained(args.text_backbone)
-        self._text_encoder = CommonsenseTextEncoder(args)
-        self._commonsense_fusion = CommonsenseInferenceFusion(args)
-        self._textual_fusion = TextualCommonsenseEnrichment(args)
-        self._visual_fusion = VisualCommonsenseEnrichment(args)
-        self._mfa = MFA(args)
-
-        self._pooler = BertPooler(self._config)
-        self._dropout = nn.Dropout(self._config.hidden_dropout_prob)
-        self._classifier = nn.Linear(self._config.hidden_size, args.num_labels)
-
-        self._temperature = args.temp
+        self.pooler = BertPooler(self.config)
+        self.dropout = nn.Dropout(self.config.hidden_dropout_prob)
+        self.classifier = nn.Linear(self.config.hidden_size, args.num_labels)
+        
+        self.temp = args.temp
 
     def contrastive_loss(self, feats_1, feats_2):
         feats_1 = feats_1.mean(dim=1)
@@ -212,18 +196,20 @@ class MICK(nn.Module):
         con_loss = - (loss_i + loss_j) / 2 
         return con_loss
     
-    def forward(self, text_feats, video_feats, audio_feats, x_react_comet_feats, x_want_comet_feats, x_react_sbert_feats, x_want_sbert_feats, caption_feats, x_before_feats, x_after_feats):
+    def forward(self, text_feats, video_feats, audio_feats, xReact_comet_feats, xWant_comet_feats, xReact_sbert_feats, xWant_sbert_feats, caption_feats, xBefore_feats, xAfter_feats):
         text_mask = text_feats[:, 1]
         
-        text_embedding, x_react_comet_embedding, x_want_comet_embedding, x_react_sbert_embedding, x_want_sbert_embedding, caption_embedding, x_before_embedding, x_after_embedding = self.text_encoder(text_feats, x_react_comet_feats, x_want_comet_feats, x_react_sbert_feats, x_want_sbert_feats, caption_feats, x_before_feats, x_after_feats)
+        text_emb, xReact_comet_emb, xWant_comet_emb, xReact_sbert_emb, xWant_sbert_emb, caption_emb, xBefore_emb, xAfter_emb = \
+            self.text_encoder(text_feats, xReact_comet_feats, xWant_comet_feats, xReact_sbert_feats, xWant_sbert_feats, caption_feats, xBefore_feats, xAfter_feats)
 
-        new_x_react_encoder_outputs, new_x_want_encoder_outputs, new_x_visual_comet_outputs = self.commonsense_fusion(text_embedding, x_react_comet_embedding, x_want_comet_embedding, x_react_sbert_embedding, x_want_sbert_embedding, x_before_embedding, x_after_embedding)
+        new_xReact_encoder_outputs_utt, new_xWant_encoder_outputs_utt, new_xVisual_comet_outputs_utt = self.commonsense_fusion(text_emb, xReact_comet_emb, xWant_comet_emb, xReact_sbert_emb, xWant_sbert_emb, xBefore_emb, xAfter_emb)
 
-        text_feats_no_caption = self.textual_fusion(text_embedding, new_x_react_encoder_outputs, new_x_want_encoder_outputs)
+        text_feats_no_caption = self.textual_fusion(text_emb, new_xReact_encoder_outputs_utt, new_xWant_encoder_outputs_utt)
+        text_with_visual_comet = self.visual_fusion(text_feats_no_caption, new_xVisual_comet_outputs_utt)
 
-        text_with_visual_comet = self.visual_fusion(text_feats_no_caption, new_x_visual_comet_outputs)
-
-        output = self.mfa(text_with_visual_comet, video_feats, audio_feats, text_mask)
+        output = self.pooler(self.mfa(text_with_visual_comet, video_feats, audio_feats, text_mask))
 
         output = self.dropout(output)
         logits = self.classifier(output)
+  
+        return logits
